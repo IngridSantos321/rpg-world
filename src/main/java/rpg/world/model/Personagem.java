b201052649f66e6435991b5cacc285fa31de102e
GuiRuchell <ruchellguilherme100@gmail.com>
2025-04-02 15:55:08 -0300

    adicionando modelo para criação de personagens

---
+++ src/main/java/rpg/world/model/Personagem.java
@@ -0,0 +1,59 @@
+package rpg.world.model;
+
+public class Personagem {
+
+   private Long id;
+   private String nome;
+   private String raca;
+   private String classe;
+   private String historia;
+   private String nivel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+}
