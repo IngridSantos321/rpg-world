12efea1f5a1309fc4a6bd88b0a3e220972ad404e
GuiRuchell <ruchellguilherme100@gmail.com>
2025-07-18 23:05:35 -0300

    adicionando melhorias no projeto rpg

--- src/main/java/rpg/world/WorldApplication.java
+++ src/main/java/rpg/world/WorldApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorldApplication {
 
-
-
+        public static void main(String[] args) {
+            SpringApplication.run(WorldApplication.class, args);
+        }
 }
--- src/main/java/rpg/world/model/Personagem.java
+++ src/main/java/rpg/world/model/Personagem.java
@@ -1,7 +1,14 @@
 package rpg.world.model;
 
-public class Personagem {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Personagem {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
    private Long id;
    private String nome;
    private String raca;
